3cd440e5e8c1f21874f9cac71803b5ed375bf983
Rajith Delantha <rajithgama@gmail.com>
2012-12-23 12:36:38 +0530

    initial commit

---
+++ src/main/webapp/resources/js/spring-mvc-exception.js
@@ -0,0 +1,16 @@
+function exception2()
+{
+    $.ajax({
+        type: 'GET',
+        url: "/exception2",
+        success: function(data)
+        {
+            $('#message1').empty().html(data).show();
+        },
+        error:function(data)
+        {
+            alert('error');
+        }
+    });
+    return false;
+}
